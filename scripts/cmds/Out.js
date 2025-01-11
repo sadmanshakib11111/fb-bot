5baf06ded41b558334711b8ad254150a2400c483
rudroahmed080 <rudroahmed080@gmail.com>
2025-01-11 15:15:48 +0600

    Create Out.js

---
+++ scripts/cmds/Out.js
@@ -0,0 +1,21 @@
+module.exports = {
+  config: {
+    name: "out",
+    aliases: ["l"],
+    version: "1.0",
+    author: "Alfred",
+    countDown: 5,
+    role: 2,
+    description: {
+      en: "remove bot from the group"
+    },
+    category: "owner",
+    guide: {
+      en: ""
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+  }
+};
