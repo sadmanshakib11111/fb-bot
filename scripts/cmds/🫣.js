4f45cfe660f9f4df9841fafe254619936c317070
Rasin <153258414+rasinislam@users.noreply.github.com>
2025-04-09 22:50:13 +0600

    Create 🫣.js

---
+++ scripts/cmds/🫣.js
@@ -0,0 +1,41 @@
+module.exports = {
+    config: {
+      name: "🫣",
+      version: "1.0",
+      author: "Rasin",
+      countDown: 0,
+      role: 0,
+      shortDescription: {
+        en: "No prefix video"
+      },
+      category: "no prefix",
+      guide: {
+        en: "No prefix"
+      },
+      hide: true
+    },
+  
+    onChat: async function ({ event, api }) {
+      const { threadID, messageID, body } = event;
+  
+      if (!body) return;
+  
+      const triggerEmojis = ["🫣"];
+      if (triggerEmojis.includes(body.trim())) {
+        const videoUrl = [
+          "https://i.imgur.com/1qS4jxx.mp4"
+        ];
+  
+        const randomVid = videoUrl[Math.floor(Math.random() * videoUrl.length)];
+  
+        return api.sendMessage(
+          {
+            body: "😌❤️‍🩹",
+            attachment: await global.utils.getStreamFromURL(randomVid)
+          },
+          threadID,
+          messageID
+        );
+      }
+    }
+  };
